e219a94f292bfb02ff0d954ea60221a7c8f81b58
Mike Alicea <malicea@meetup.com>
2020-05-08 14:42:18 -0400

    Typed AWS methods

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -3,43 +3,51 @@ import App from 'next/app';
 import { withRouter } from 'next/router';
 import Head from 'next/head';
 import { Auth } from 'aws-amplify';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import type {
+  CurrentAuthenticatedUser,
+  ForgotPassword,
+  ConfirmSignUp,
+} from '../types/amplify.d';
+import 'antd/dist/antd.css';
 import '../styles.scss';
 
 import UserContext from '../state-management/UserContext';
 import initAmplifyAuth from '../state-management/cognitoConfig';
 
 type State = {
-  user: {
+  user?: {
     email: string;
     firstName: string;
     lastName: string;
-  } | null;
-  userIsLoaded: boolean;
-  jwtToken: string | null;
+  };
+  userIsLoaded?: boolean;
+  jwtToken?: string;
 };
 class MyApp extends App<{}, State> {
-  state = {
-    user: null,
+  state: State = {
+    user: undefined,
     userIsLoaded: false,
-    jwtToken: null,
+    jwtToken: undefined,
   };
 
   async componentDidMount(): Promise<void> {
     initAmplifyAuth();
-    await this.restoreSession();
-    await this.restoreUser();
+    try {
+      await this.restoreSession();
+      await this.restoreUser();
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  // eslint-disable-next-line
   register = async (
     username: string,
     password: string,
     firstName: string,
     lastName: string
-  ) => {
+  ): Promise<State['user']> => {
     try {
-      const auth = await Auth.signUp({
+      await Auth.signUp({
         username,
         password,
         attributes: {
@@ -57,14 +65,14 @@ class MyApp extends App<{}, State> {
       };
 
       this.setState({ user });
-      return auth;
+      return user;
     } catch (error) {
       console.error('register', error);
       if (error.code === 'UsernameExistsException') {
         alert(`register: A user with this email already exists`);
       }
 
-      throw error;
+      return error;
     }
   };
 
@@ -72,11 +80,9 @@ class MyApp extends App<{}, State> {
     username: string,
     code: string,
     routeOnSuccess?: string
-    // @TODO type this
-    // eslint-disable-next-line
-  ): Promise<any> => {
+  ): Promise<ConfirmSignUp> => {
     try {
-      const auth = await Auth.confirmSignUp(username, code);
+      const auth: ConfirmSignUp = await Auth.confirmSignUp(username, code);
       if (routeOnSuccess) {
         this.props.router.push(routeOnSuccess);
       }
@@ -86,23 +92,25 @@ class MyApp extends App<{}, State> {
       if (error.code !== 'CodeMismatchException') {
         alert(`confirmEmail: ${error.message}`);
       }
-      throw error;
+      return error;
     }
   };
 
   resendRegisterCode = async (username: string): Promise<boolean> => {
     try {
-      await Auth.resendSignUp(username);
-      return true;
+      // type is ../../types/amplify.d.ts.ResendSignUp
+      const resendSignUp = await Auth.resendSignUp(username);
+      return Boolean(resendSignUp);
     } catch (error) {
       console.error('resendRegisterCode', error);
       return error;
     }
   };
 
-  // @TODO type this
-  // eslint-disable-next-line
-  signIn = async (username: string, password: string) => {
+  signIn = async (
+    username: string,
+    password: string
+  ): Promise<State['user']> => {
     try {
       const cognitoUser = await Auth.signIn({
         username,
@@ -119,51 +127,59 @@ class MyApp extends App<{}, State> {
         user: userInfo,
       });
 
-      return cognitoUser;
+      return userInfo;
     } catch (error) {
       console.error('signIn', error);
       if (error.code === 'UserNotConfirmedException') {
         alert(`signIn: ${error.message}`);
       }
-      throw error;
+      return error;
     }
   };
 
-  // @TODO type this
-  // eslint-disable-next-line
-  signOut = async (): Promise<any> => {
+  signOut = async (): Promise<boolean> => {
     try {
-      const auth = await Auth.signOut();
+      await Auth.signOut();
       this.setState({
         user: null,
         userIsLoaded: true,
         jwtToken: null,
       });
       this.props.router.push('/');
-      return auth;
+      return true;
     } catch (error) {
       console.error('signOut', error);
-      throw error;
+      return error;
     }
   };
 
-  restoreSession = async (): Promise<void> => {
+  restoreSession = async (): Promise<string> => {
     try {
       const userSession = await Auth.currentSession();
       const jwtToken = userSession.getIdToken().getJwtToken();
       this.setState({ userIsLoaded: true, jwtToken });
+      return jwtToken;
     } catch (error) {
       console.error('restoreSession', error);
+      return error;
     }
   };
 
-  restoreUser = async (): Promise<void> => {
+  restoreUser = async (): Promise<State['user']> => {
     try {
-      const user = await Auth.currentAuthenticatedUser();
+      const currentAuthenticatedUser: CurrentAuthenticatedUser = await Auth.currentAuthenticatedUser();
+      const { attributes } = currentAuthenticatedUser;
+      const user = {
+        firstName: attributes.given_name,
+        lastName: attributes.family_name,
+        email: attributes.email,
+      };
       this.setState({
         user,
         userIsLoaded: true,
       });
+
+      return user;
     } catch (error) {
       console.error('restoreUser', error);
       if (error === 'not authenticated') {
@@ -171,18 +187,17 @@ class MyApp extends App<{}, State> {
           userIsLoaded: true,
         });
       }
+      return error;
     }
   };
 
-  // @TODO type this
-  // eslint-disable-next-line
-  forgotPasswordInit = async (username: string): Promise<any> => {
+  forgotPasswordInit = async (username: string): Promise<boolean> => {
     try {
-      const sent = await Auth.forgotPassword(username);
-      return sent;
+      const sent: ForgotPassword = await Auth.forgotPassword(username);
+      return Boolean(sent.CodeDeliveryDetails);
     } catch (error) {
       console.error(error);
-      throw error;
+      return error;
     }
   };
 
@@ -190,15 +205,13 @@ class MyApp extends App<{}, State> {
     username: string,
     code: string,
     newPassword: string
-    // @TODO type this
-    // eslint-disable-next-line
-  ): Promise<any> => {
+  ): Promise<boolean> => {
     try {
       const sent = await Auth.forgotPasswordSubmit(username, code, newPassword);
-      return sent;
+      return Boolean(sent);
     } catch (error) {
       console.error(error);
-      throw error;
+      return error;
     }
   };
 
---
+++ packages/frontend/src/types/amplify.d.ts
@@ -0,0 +1,58 @@
+export interface CurrentAuthenticatedUser {
+  Session: null;
+  attributes: {
+    email: string;
+    email_verified: boolean;
+    family_name: string;
+    given_name: string;
+    sub: string;
+  };
+  username: string; // Is the actuaal key in the cognito table
+}
+
+export interface ForgotPassword {
+  CodeDeliveryDetails: {
+    AttributeName: string;
+    DeliveryMedium: string;
+    Destination: string;
+  };
+  code?: 'LimitExceededException';
+  message?: 'Attempt limit exceeded, please try after some time.';
+  name?: 'LimitExceededException';
+}
+
+export type ConfirmSignUp =
+  | 'SUCCESS'
+  | {
+      code: 'CodeMismatchException';
+      name: 'CodeMismatchException';
+      message: 'Invalid verification code provided; please try again.';
+    };
+
+export interface ResendSignUp {
+  CodeDeliveryDetails: {
+    AttributeName: 'email';
+    DeliveryMedium: 'EMAIL';
+    Destination: 'm***@g***.com';
+  };
+}
+
+export interface SignUp {
+  codeDeliveryDetails: {
+    AttributeName: 'email';
+    DeliveryMedium: 'EMAIL';
+    Destination: string;
+  };
+  user: {
+    Session: string | null;
+    authenticationFlowType: 'USER_SRP_AUTH';
+    client: Record<string, any>;
+    pool: Record<string, any>;
+    signInUserSession: Record<string, any> | null;
+    storage: Record<string, any>;
+    userDataKey: string;
+    username: string;
+  };
+  userConfirmed: boolean;
+  userSub: string;
+}
