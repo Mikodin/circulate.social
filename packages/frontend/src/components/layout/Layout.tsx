e50ed61c70589cfa081ad226c0e0b0c51053ff2f
Mike Alicea <mfalicea58@gmail.com>
2020-05-31 14:09:26 -0700

    Fix Layout filename and types

---
+++ packages/frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import { Fragment, PropsWithChildren } from 'react';
+import Head from 'next/head';
+
+import css from './Layout.module.scss';
+import Header from '../header/Header';
+
+const Layout = (props: PropsWithChildren<{}>): JSX.Element => (
+  <Fragment>
+    <Head>
+      <title>Circulate</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Header />
+    <div className={css.Layout}>{props.children}</div>
+  </Fragment>
+);
+
+export default Layout;
