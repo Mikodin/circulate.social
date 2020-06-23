3f4ec34a95bc7ecd8c07473be4c5048e49b95101
Mike Alicea <mfalicea58@gmail.com>
2020-06-23 08:26:22 -0700

    Remove uneccessary log

--- packages/api/src/endpoints/createContent/createContent.ts
+++ packages/api/src/endpoints/createContent/createContent.ts
@@ -72,7 +72,6 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       memberId,
       circleId
     );
-    console.log(circlesUserIsMemberOf);
 
     if (!circlesUserIsMemberOf.length) {
       return generateReturn(404, {
