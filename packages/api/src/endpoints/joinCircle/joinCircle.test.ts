53a47ec7fe602d3776b1436441fed996ae296272
Mike Alicea <mfalicea58@gmail.com>
2020-06-23 08:27:21 -0700

    joinCircle is at 100% on dynamoose

--- packages/api/src/endpoints/joinCircle/joinCircle.test.ts
+++ packages/api/src/endpoints/joinCircle/joinCircle.test.ts
@@ -1,3 +1,113 @@
-it('should pass', () => {
-  expect(true).toBe(true);
+import log from 'lambda-log';
+import { handler } from './joinCircle';
+
+import { CreateMockEvent, CreateMockContext } from '../testUtils';
+import CircleModel from '../../interfaces/dynamo/circlesModel';
+
+jest.mock('../../interfaces/dynamo/circlesModel', () => ({
+  update: jest.fn(() =>
+    Promise.resolve({
+      original: () => ({ id: 'asdf-123' }),
+    })
+  ),
+}));
+
+const MOCK_EVENT_CONTEXT = {
+  authorizer: {
+    claims: {
+      'cognito:username': 'dev-id',
+      email_verified: true,
+    },
+  },
+};
+
+const mockCircleIdPathParameter = 'asdf-123';
+function genMockEvent(
+  pathParameters?: Record<string, string>,
+  eventContext?: Record<string, unknown>
+) {
+  return CreateMockEvent({
+    pathParameters: { circleId: mockCircleIdPathParameter, ...pathParameters },
+    requestContext: { ...MOCK_EVENT_CONTEXT, ...eventContext },
+  });
+}
+
+const MOCK_EVENT = genMockEvent();
+const MOCK_CONTEXT = CreateMockContext({});
+
+describe('joinCircle', () => {
+  describe('Happy path', () => {
+    it('Should call CircleModel.update with the pathParameters.circleId', async () => {
+      const circleModelUpdateSpy = jest.spyOn(CircleModel, 'update');
+      await handler(MOCK_EVENT, MOCK_CONTEXT, null);
+      expect(circleModelUpdateSpy).toHaveBeenCalledWith(
+        {
+          id: 'asdf-123',
+        },
+        { $ADD: { members: ['dev-id'] } }
+      );
+    });
+    it('Should return a 200 with { success: true, circleId: pathParameters.circleId }', async () => {
+      const resp = await handler(MOCK_EVENT, MOCK_CONTEXT, null);
+      expect(resp).toEqual(
+        expect.objectContaining({
+          statusCode: 200,
+          body:
+            '{"message":"Success","joined":{"success":true,"circleId":"asdf-123"}}',
+        })
+      );
+    });
+  });
+
+  describe('Unhappy path', () => {
+    describe('When the user has not confirmed their email', () => {
+      const emailNotVerifiedEventAuthorizer = {
+        authorizer: {
+          claims: {
+            'cognito:username': 'dev-id',
+            email_verified: false,
+          },
+        },
+      };
+      it('Should return a 401', async () => {
+        const resp = await handler(
+          genMockEvent(undefined, emailNotVerifiedEventAuthorizer),
+          MOCK_CONTEXT,
+          null
+        );
+        expect(resp).toEqual(
+          expect.objectContaining({
+            statusCode: 401,
+            body: '{"message":"Please verify your email address"}',
+          })
+        );
+      });
+    });
+  });
+
+  describe('Catastrophic path', () => {
+    describe('When CircleModel.update throws an error', () => {
+      const circleModelUpdateSpy = jest.spyOn(CircleModel, 'update');
+      beforeEach(() => {
+        circleModelUpdateSpy.mockImplementationOnce(() =>
+          Promise.reject(false)
+        );
+      });
+      it('Should return a 500', async () => {
+        const resp = await handler(MOCK_EVENT, MOCK_CONTEXT, null);
+        expect(resp).toEqual(
+          expect.objectContaining({
+            statusCode: 500,
+            body:
+              '{"message":"Something went wrong trying to join the circle"}',
+          })
+        );
+      });
+      it('Should call log.error', async () => {
+        const logErrorSpy = jest.spyOn(log, 'error');
+        await handler(MOCK_EVENT, MOCK_CONTEXT, null);
+        expect(logErrorSpy).toHaveBeenCalled();
+      });
+    });
+  });
 });
--- packages/api/src/endpoints/joinCircle/joinCircle.ts
+++ packages/api/src/endpoints/joinCircle/joinCircle.ts
@@ -1,60 +1,38 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import log from 'lambda-log';
-import { joinCircle } from '../../interfaces/dynamo/circlesTable';
+import { Circle } from '@circulate/types/index';
+import CircleModel from '../../interfaces/dynamo/circlesModel';
+import { generateReturn, getMemberFromAuthorizer } from '../endpointUtils';
 
 import 'source-map-support/register';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-  const isInLocal = process.env.IS_LOCAL === 'true';
-  const memberId = isInLocal
-    ? 'dev-id'
-    : event.requestContext.authorizer.claims['cognito:username'];
-  const isEmailVerified = isInLocal
-    ? true
-    : event.requestContext.authorizer.claims.email_verified;
-
+  const { memberId, isEmailVerified } = getMemberFromAuthorizer(event);
   const { circleId } = event.pathParameters;
 
   if (!isEmailVerified) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({
-        message: 'Please verify your email address',
-      }),
-      headers: {
-        // @TODO limit to only my domain
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-    };
+    return generateReturn(401, {
+      message: 'Please verify your email address',
+    });
   }
 
   try {
-    const joined = await joinCircle(circleId, memberId);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'Success',
-        joined,
-      }),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-    };
+    const updatedCircle = (
+      await CircleModel.update(
+        { id: circleId },
+        { $ADD: { members: [memberId] } }
+      )
+    ).original() as Circle;
+    return generateReturn(200, {
+      message: 'Success',
+      joined: { success: true, circleId: updatedCircle.id },
+    });
   } catch (error) {
     log.error('Failed to join circle', {
       error,
     });
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: 'Something went wrong trying to join the circle',
-      }),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-    };
+    return generateReturn(500, {
+      message: 'Something went wrong trying to join the circle',
+    });
   }
 };
