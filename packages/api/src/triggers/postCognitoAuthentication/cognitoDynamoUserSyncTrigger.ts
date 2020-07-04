88eb10f88870f4cb76771ba826042aab8c7781b3
Mike Alicea <mfalicea58@gmail.com>
2020-07-04 13:08:33 -0700

    Use create instead of save.  Overwrite=true

--- packages/api/src/triggers/postCognitoAuthentication/cognitoDynamoUserSyncTrigger.ts
+++ packages/api/src/triggers/postCognitoAuthentication/cognitoDynamoUserSyncTrigger.ts
@@ -11,21 +11,26 @@ export const handler: CognitoUserPoolTriggerHandler = async (
   log.info(event);
   const { request } = event;
   const { userAttributes } = request;
-  const user = new UserModel({
-    id: userAttributes.sub,
+  const user = {
+    id: `${userAttributes.sub}`,
     email: userAttributes.email,
     firstName: userAttributes['custom:first_name'],
     lastName: userAttributes['custom:last_name'],
-  });
+  };
 
   try {
     log.info('Attempting to create user', { user });
-    const savedUser = await user.save();
+
+    // @ts-expect-error
+    const savedUser = await UserModel.create(user, { overwrite: true });
+
     log.info('Upserted user', user);
 
     return { ...event, user: savedUser };
   } catch (error) {
+    // Don't throw error.  We don't want to block a user from signing in because of this
     log.error('Failed to save user', error);
   }
+
   return event;
 };
