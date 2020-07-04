6261b1b0c2d267d37a635e1df8c5fd775eae59ba
Mike Alicea <mfalicea58@gmail.com>
2020-07-04 13:52:22 -0700

    Adds User type

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,4 +1,5 @@
 export type { Event } from './src/event';
 export type { Circle } from './src/circle';
 export type { Content } from './src/content';
+export type { User } from './src/user';
 export type { AntdMoment } from './src/antdMoment';
