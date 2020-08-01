81d3af637afe3606822fc1115a31339befb9d6a8
Mike Alicea <mfalicea58@gmail.com>
2020-08-01 10:58:14 -0700

    Adds describe blocks.  TODO: Implement these tests

---
+++ packages/api/src/circulationSender/triggers/contentTable/buildCirculationHelpers.test.ts
@@ -0,0 +1,9 @@
+// TODO: Implement these tests
+describe.skip('buildCirculationHelpers', () => {
+  it(
+    'Should be implemented, but for time - I will skip these tests.  The logic is "covered" in buildCirculationTable.test albeit, missing some edge cases'
+  );
+  describe('fetchCircles', () => undefined);
+  describe('updateOrCreateCirculation', () => undefined);
+  describe('constructCirculationArray', () => undefined);
+});
