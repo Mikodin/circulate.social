648a66142bdf422f75a72908e7111a62531649b2
Mike Alicea <mfalicea58@gmail.com>
2020-08-16 20:50:34 -0700

    Seperates events from posts - needs cleanup

--- packages/api/src/circulationSender/circulationHtmlConstructor.helper.ts
+++ packages/api/src/circulationSender/circulationHtmlConstructor.helper.ts
@@ -9,21 +9,33 @@ export const createCirculationHtmlForUser = (
   usersFirstName: string,
   circulation: Circulation
 ): string => {
+  // TODO This should be moved elsewhere.  It's too hidden and happens in one place.
+  // Code is hard to understand
+  // Should events be seperate from posts?
+  const upcomingEvents = Array.from(circulation.circleDetails).flatMap(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    ([_, circle]) =>
+      circle.contentDetails
+        .filter((content) => content.dateTime)
+        .map((event) => ({ ...event, circle }))
+  );
+
   const circleDetailsArray = Array.from(circulation.circleDetails).map(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     ([_, val]) => {
       const upcomingPosts = val.contentDetails.filter(
         (content) => !content.dateTime
       );
-      const upcomingEvents = val.contentDetails.filter((content) =>
-        Boolean(content.dateTime)
-      );
 
-      return { ...val, upcomingPosts, upcomingEvents };
+      return { ...val, upcomingPosts };
     }
   );
   return template({
     usersFirstName,
-    circulation: { ...circulation, circleDetails: circleDetailsArray },
+    circulation: {
+      ...circulation,
+      circleDetails: circleDetailsArray,
+      upcomingEvents,
+    },
   });
 };
--- packages/api/src/circulationSender/circulationTemplate.ts
+++ packages/api/src/circulationSender/circulationTemplate.ts
@@ -7,22 +7,21 @@ export default `
 <p>Pause all Emails</p>
 
     {{#with circulation}}
+        <h4>Events</h4>
+        {{#each upcomingEvents}}
+            <div>
+                <p style="margin: 0; margin-bottom: 5px">
+                    {{title}} | #{{circle.name}} | {{createdBy}}
+                </p>
+                <span style="margin-left: 5px;">{{description}}</span>
+            </div>
+        {{/each}}
+
         {{#each circleDetails}}
             <h3 style="margin: 0; padding: 0; margin-bottom: 5px;">
                 {{name}}
             </h3>
             <div style="margin-left: 5px;">
-
-                <h4>Events</h4>
-                {{#each upcomingEvents}}
-                    <div>
-                        <p style="margin: 0; margin-bottom: 5px">
-                            {{title}} | {{createdBy}}
-                        </p>
-                        <span style="margin-left: 5px;">{{description}}</span>
-                    </div>
-                {{/each}}
-
                 <h4>Posts</h4>
                 {{#each upcomingPosts}}
                     <div>
