2a9623df8ccad88c43157ce2fff1651d98657a12
Mike Alicea <mfalicea58@gmail.com>
2020-08-29 14:21:56 -0400

    Put prod back to using prod - whoops!

--- packages/frontend/src/util/constants.ts
+++ packages/frontend/src/util/constants.ts
@@ -4,5 +4,6 @@ export const PROD_ENDPOINT =
 export const DEV_ENDPOINT =
   'https://z3edrz53yg.execute-api.us-east-1.amazonaws.com/dev';
 
+export const LOCAL_ENDPOINT = 'http://localhost:3000/dev';
+
 export const API_ENDPOINT = PROD_ENDPOINT;
-// export const API_ENDPOINT = 'http://localhost:3000/dev';
