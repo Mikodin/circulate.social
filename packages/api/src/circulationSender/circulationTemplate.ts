ef67611715de2a869b6c9e6e0359d032fdc864cc
Mike Alicea <mfalicea58@gmail.com>
2020-10-26 12:46:10 -0700

    Breaking template out into it's own functions!

--- packages/api/src/circulationSender/circulationTemplate.ts
+++ packages/api/src/circulationSender/circulationTemplate.ts
@@ -1,71 +1,87 @@
+function generateLegend() {
+  const genLineItem = (text: string) =>
+    `<p style="font-size: 10px; margin: 0">${text}</p>`;
+
+  return `
+<div>
+    ${genLineItem(
+      `Click the ☆ to save to your Circulate Event and Post Archive and have it automatically go to your calendar.`
+    )}
+    ${genLineItem(`Submit a Post`)}
+    ${genLineItem(`Leave a Circle`)}
+    ${genLineItem(`Pause all Emails`)}
+</div>`;
+}
+
+function generatePost(isEvent: boolean) {
+  const circleName = isEvent ? '{{circle.name}}' : '{{name}}';
+  const circleId = isEvent ? '{{circle.id}}' : '{{id}}';
+
+  return `
+  <div style="margin-bottom:5px;>
+    {{#if link}}
+        <p style="margin: 0;">
+            ☆ <a href={{{link}}}>{{title}}</a> | {{createdBy}} | <a href=https://beta.circulate.social/circles/${circleId}>${circleName}</a>${
+    isEvent ? ' | {{time}}' : ''
+  }
+        </p>
+    {{/if}}
+    {{#unless link}}
+        <p style="margin: 0;">
+            ☆ {{title}} | {{createdBy}} | <a href=https://beta.circulate.social/circles/${circleId}>${circleName}</a>${
+    isEvent ? ' | {{time}}' : ''
+  }
+        </p>
+    {{/unless}}
+
+    {{#if description}}
+        <p style="margin:0; margin-left: 25px;">{{description}}?</p>
+    {{/if}}
+</div>`;
+}
+
+function generateUpcomingEvents() {
+  return `
+  <h3>Upcoming events</h3>
+    {{#each upcomingEvents}}
+        <div style="margin-bottom: 10px;">
+            <h4 style="margin:0; margin-bottom: 5px;">{{dateTimeString}}</h4>
+            {{#each events}}
+                ${generatePost(true)}
+            {{/each}}
+        </div>
+    {{/each}}`;
+}
+
+function generateUpcomingPosts() {
+  return `
+  <h3>Your Circles posts</h3>
+    {{#each circleDetails}}
+        {{#if upcomingPosts}}
+            <h4 style="margin: 0; margin-bottom: 5px;">
+                <a href=https://beta.circulate.social/circles/{{id}}>{{name}}</a>
+            </h4>
+            <div style="margin-left: 5px;">
+                <h5 style="margin: 0;">Posts</h5>
+                {{#each upcomingPosts}}
+                   ${generatePost(false)} 
+                {{/each}}
+            </div>
+        {{/if}}
+    {{/each}}`;
+}
+
 export default `
 <div>
-<p style="font-size: 10px; margin: 0">Here is today's curation from all of your Circles.</p>
-<br />
-<p style="font-size: 10px; margin: 0">Click the ☆ to save to your Circulate Event and Post Archive and have it automatically go to your calendar</p>
-<p style="font-size: 10px; margin: 0">Submit a Post</p>
-<p style="font-size: 10px; margin: 0">Leave a Circle</p>
-<p style="font-size: 10px; margin: 0">Pause all Emails</p>
-
+    <h3>Here is today's curation from all of your Circles.</h3>
+    ${generateLegend()}
     {{#with circulation}}
         {{#if hasEventsToSend}}
-            <h3>Upcoming events</h3>
-            {{#each upcomingEvents}}
-                <div style="margin-bottom: 10px;">
-                    <h4 style="margin:0; margin-bottom: 5px;">{{dateTimeString}}</h4>
-                    {{#each events}}
-                        <div style="margin-bottom:5px;>
-                            {{#if link}}
-                                <p style="margin: 0;">
-                                    ☆ <a href={{{link}}}>{{title}}</a> | {{createdBy}} | <a href=https://beta.circulate.social/circles/{{circle.id}}>{{circle.name}}</a> | {{time}}
-                                </p>
-                            {{/if}}
-                            {{#unless link}}
-                                <p style="margin: 0;">
-                                    ☆ {{title}} | {{createdBy}} | <a href=https://beta.circulate.social/circles/{{circle.id}}>{{circle.name}}</a> | {{time}}
-                                </p>
-                            {{/unless}}
-
-                            {{#if description}}
-                                <p style="margin:0; margin-left: 25px;">{{description}}?</p>
-                            {{/if}}
-                        </div>
-                    {{/each}}
-                </div>
-            {{/each}}
+            ${generateUpcomingEvents()}
         {{/if}}
         <hr />
-
         {{#if hasPostsToSend}}
-        <h3>Your Circles posts</h3>
-            {{#each circleDetails}}
-                {{#if upcomingPosts}}
-                    <h4 style="margin: 0; margin-bottom: 5px;">
-                        <a href=https://beta.circulate.social/circles/{{id}}>{{name}}</a>
-                    </h4>
-                    <div style="margin-left: 5px;">
-                        <h5 style="margin: 0;">Posts</h5>
-                        {{#each upcomingPosts}}
-                            <div style="margin-bottom: 5px;">
-                                {{#if link}}
-                                    <p style="margin:0;">
-                                    ☆ <a href={{{link}}}>{{title}}</a> | {{createdBy}}
-                                    </p>
-                                {{/if}}
-                                {{#unless link}}
-                                    <p style="margin:0;">
-                                    ☆ {{title}} | {{createdBy}}
-                                    </p>
-                                {{/unless}}
-
-                                {{#if description}}
-                                    <p style="margin:0; margin-left: 25px;">{{description}}</p>
-                                {{/if}}
-                            </div>
-                        {{/each}}
-                    </div>
-                {{/if}}
-            {{/each}}
+            ${generateUpcomingPosts()}
         {{/if}}
     {{/with}}
 </div>`;
