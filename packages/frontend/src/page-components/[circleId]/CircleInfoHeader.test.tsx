6369703ee492c90fbebf045e77eab704993e5046
Mike Alicea <mfalicea58@gmail.com>
2020-11-02 18:19:18 -0800

    Update CircleInfoHeader to deal with CirclePreview

--- packages/frontend/src/page-components/[circleId]/CircleInfoHeader.test.tsx
+++ packages/frontend/src/page-components/[circleId]/CircleInfoHeader.test.tsx
@@ -1,19 +1,36 @@
 import { render, RenderResult } from '@testing-library/react';
+import { Circle, CirclePreview } from '@circulate/types';
+
 import CircleInfoHeader, { Props } from './CircleInfoHeader';
 
+const mockCircle = {
+  id: 'asdf-123',
+  createdAt: '1234',
+  updatedAt: '12345',
+  members: ['Mike A', 'Bill Nye'],
+  createdBy: '1234-asdf',
+  name: 'The greatest Circle ever',
+  description: 'A circle for amazing things',
+  content: ['content-1', 'content-2'],
+  frequency: 'weekly' as const,
+  privacy: 'private' as const,
+} as Circle;
+
+const mockCirclePreview = {
+  circleId: 'asdf-123',
+  createdAt: '1234',
+  updatedAt: '12345',
+  memberNames: ['Mike A', 'Bill Nye'],
+  creatorName: '1234-asdf',
+  name: 'The greatest Circle ever',
+  description: 'A circle for amazing things',
+  totalContentCount: 2,
+  frequency: 'weekly' as const,
+  privacy: 'private' as const,
+} as CirclePreview;
+
 const DEFAULT_PROPS = {
-  circle: {
-    id: 'asdf-123',
-    createdAt: '1234',
-    updatedAt: '12345',
-    members: ['Mike A', 'Bill Nye'],
-    createdBy: '1234-asdf',
-    name: 'The greatest Circle ever',
-    description: 'A circle for amazing things',
-    content: ['content-1', 'content-2'],
-    frequency: 'weekly' as const,
-    privacy: 'private' as const,
-  },
+  circle: mockCircle,
   isLoading: false,
   jwtToken: 'some-token',
 };
@@ -27,29 +44,82 @@ describe('CircleInfoHeader', () => {
     expect(renderCircleInfoHeader()).toMatchSnapshot();
   });
 
-  it('Should contain the count of content under Posts', () => {
-    const { queryByText } = renderCircleInfoHeader();
-    expect(queryByText(/Posts: 2/i)).toBeTruthy();
-  });
-  it('Should contain the count of Members', () => {
-    const { queryByText } = renderCircleInfoHeader();
-    expect(queryByText(/Members: 2/i)).toBeTruthy();
-  });
-  it('Should contain the description from the Circle', () => {
-    const { queryByText } = renderCircleInfoHeader();
-    expect(queryByText(DEFAULT_PROPS.circle.description)).toBeTruthy();
-  });
-  it('Should contain the frequency that the Circle sends with', () => {
-    const { queryByText } = renderCircleInfoHeader();
-    expect(queryByText(/Weekly/i)).toBeTruthy();
+  describe('When given a Circle', () => {
+    it('Should contain any CircleActions', () => {
+      const { queryByText } = renderCircleInfoHeader();
+      expect(queryByText(/Invite/i)).toBeTruthy();
+    });
+    it('Should contain the count of content under Posts', () => {
+      const { queryByText } = renderCircleInfoHeader();
+      expect(queryByText(/Posts: 2/i)).toBeTruthy();
+    });
+    it('Should contain the count of Members', () => {
+      const { queryByText } = renderCircleInfoHeader();
+      expect(queryByText(/Members: 2/i)).toBeTruthy();
+    });
+    it('Should contain the description from the Circle', () => {
+      const { queryByText } = renderCircleInfoHeader();
+      expect(queryByText(DEFAULT_PROPS.circle.description)).toBeTruthy();
+    });
+    it('Should contain the frequency that the Circle sends with', () => {
+      const { queryByText } = renderCircleInfoHeader();
+      expect(queryByText(/Weekly/i)).toBeTruthy();
+    });
+
+    describe('When Circle has no description', () => {
+      it('Should not contain the description from the Circle if there is no description', () => {
+        const { queryByText } = renderCircleInfoHeader({
+          circle: { ...DEFAULT_PROPS.circle, description: undefined },
+        });
+        expect(queryByText(/Description/i)).toBeFalsy();
+      });
+    });
   });
 
-  describe('When Circle has no description', () => {
-    it('Should not contain the description from the Circle if there is no description', () => {
-      const { queryByText } = renderCircleInfoHeader({
-        circle: { ...DEFAULT_PROPS.circle, description: undefined },
+  describe('When given a CirclePreview', () => {
+    const mockCirclePreviewPropOverrides = {
+      circle: undefined,
+      circlePreview: mockCirclePreview,
+    };
+    it('Should not contain any CircleActions', () => {
+      const { queryByText } = renderCircleInfoHeader(
+        mockCirclePreviewPropOverrides
+      );
+      expect(queryByText(/Invite/i)).toBeFalsy();
+    });
+    it('Should contain the count of content under Posts', () => {
+      const { queryByText } = renderCircleInfoHeader(
+        mockCirclePreviewPropOverrides
+      );
+      expect(queryByText(/Posts: 2/i)).toBeTruthy();
+    });
+    it('Should contain the count of Members', () => {
+      const { queryByText } = renderCircleInfoHeader(
+        mockCirclePreviewPropOverrides
+      );
+      expect(queryByText(/Members: 2/i)).toBeTruthy();
+    });
+    it('Should contain the description from the Circle', () => {
+      const { queryByText } = renderCircleInfoHeader(
+        mockCirclePreviewPropOverrides
+      );
+      expect(queryByText(DEFAULT_PROPS.circle.description)).toBeTruthy();
+    });
+    it('Should contain the frequency that the Circle sends with', () => {
+      const { queryByText } = renderCircleInfoHeader(
+        mockCirclePreviewPropOverrides
+      );
+      expect(queryByText(/Weekly/i)).toBeTruthy();
+    });
+
+    describe('When CirclePreview has no description', () => {
+      it('Should not contain the description from the Circle if there is no description', () => {
+        const { queryByText } = renderCircleInfoHeader({
+          circle: undefined,
+          circlePreview: { ...mockCirclePreview, description: undefined },
+        });
+        expect(queryByText(/Description/i)).toBeFalsy();
       });
-      expect(queryByText(/Description/i)).toBeFalsy();
     });
   });
 
--- packages/frontend/src/page-components/[circleId]/CircleInfoHeader.tsx
+++ packages/frontend/src/page-components/[circleId]/CircleInfoHeader.tsx
@@ -1,4 +1,4 @@
-import { Circle } from '@circulate/types';
+import { Circle, CirclePreview } from '@circulate/types';
 import { Skeleton } from 'antd';
 
 import CircleActionsContainer from '../../components/circleActions/CircleActionsContainer';
@@ -6,32 +6,44 @@ import CircleMembers from '../../components/circleMembers/CircleMembers';
 import styles from './circleInfoHeader.module.scss';
 
 export interface Props {
-  circle: Circle;
+  circle?: Circle;
+  circlePreview?: CirclePreview;
   isLoading: boolean;
   jwtToken: string;
 }
 
 const CircleInfoHeader = (props: Props): JSX.Element => {
-  const { circle, isLoading, jwtToken } = props;
-  return isLoading || !circle ? (
+  const { circle, circlePreview, isLoading, jwtToken } = props;
+  const { name, description, frequency } = circle || circlePreview || {};
+
+  let totalContentCount;
+  let circleMemberNames;
+  if (circle || circlePreview) {
+    circleMemberNames = circle ? circle.members : circlePreview.memberNames;
+    totalContentCount = circle
+      ? (circle.content || []).length
+      : circlePreview.totalContentCount;
+  }
+
+  return isLoading || (!circle && !circlePreview) ? (
     <div data-testid="skeleton">
       <Skeleton active={isLoading} />
     </div>
   ) : (
     <div className={styles.infoHeader}>
-      <h2>{circle.name}</h2>
-      <h4>Posts: {(circle.content || []).length}</h4>
+      <h2>{name}</h2>
+      <h4>Posts: {totalContentCount}</h4>
       <CircleMembers
-        circleMemberNames={circle.members}
+        circleMemberNames={circleMemberNames}
         renderComponent={
           <h4>
-            <a>Members: {circle.members.length}</a>
+            <a>Members: {circleMemberNames.length}</a>
           </h4>
         }
       />
-      <h4>Sends: {circle.frequency}</h4>
-      {circle.description && <p>{circle.description}</p>}
-      <CircleActionsContainer circle={circle} jwtToken={jwtToken} />
+      <h4>Sends: {frequency}</h4>
+      {description && <p>{description}</p>}
+      {circle && <CircleActionsContainer circle={circle} jwtToken={jwtToken} />}
     </div>
   );
 };
